6c1a8b76f8de56d86c6e22873ab3b72a0cd11e8c
Bazsó Krisztián <bazso.k12@gmail.com>
2023-03-13 21:49:57 +0100

    add tailwind and login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,81 @@
 <script setup lang="ts">
-
+//import { LockClosedIcon } from '@heroicons/vue/20/solid'
 </script>
 
 <template>
-  <main>    
-  </main>
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+      <div>
+        <img
+          class="mx-auto h-12 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=lime&shade=600"
+          alt="Your Company"
+        />
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" value="true" />
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label for="username" class="sr-only">Username</label>
+            <input
+              id="username"
+              name="username"
+              type="text"
+              :required="true"
+              class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-lime-600 sm:text-sm sm:leading-6"
+              placeholder="Username"
+            />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              :required="true"
+              class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-lime-600 sm:text-sm sm:leading-6"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+
+        <!-- <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input
+              id="remember-me"
+              name="remember-me"
+              type="checkbox"
+              class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+            />
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+          </div>
+
+          <div class="text-sm">
+            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"
+              >Forgot your password?</a
+            >
+          </div>
+        </div> -->
+
+        <div>
+          <button
+            type="submit"
+            class="group relative flex w-full justify-center rounded-md bg-lime-600 py-2 px-3 text-sm font-semibold text-white hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <LockClosedIcon
+                class="h-5 w-5 text-green-500 group-hover:text-green-400"
+                aria-hidden="true"
+              />
+            </span>
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
